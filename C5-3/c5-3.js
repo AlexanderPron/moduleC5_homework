b57e379e8e4e7282fdbb3f68456b02220854b4a1
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-09-15 00:01:07 +0300

    Add files via upload

--- C5-3/c5-3.js
+++ C5-3/c5-3.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+function getImages(){
+    console.log("Показываем картинки");
+}
+function init(){
+    document.querySelector(".sendDataButton").addEventListener("click", (e)=>{
+        e.preventDefault();
+        const value = document.querySelector(".inputData").value;
+        if (Number(value)){
+            if (Number(value) >=1 && Number(value) <= 10){
+                document.querySelector(".rezultMsg").innerHTML = "";
+                getImages();
+            }
+            else{
+                document.querySelector(".rezultMsg").innerHTML = "число вне диапазона от 1 до 10";
+            }
+        }
+        else{
+            document.querySelector(".rezultMsg").innerHTML = "Введено не число";
+        }
+    });
+}
+init();
