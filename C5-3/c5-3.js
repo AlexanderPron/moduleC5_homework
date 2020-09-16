fbf600a882f00015d635f5eeaad01f72c6ea8add
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-09-16 18:12:28 +0300

    Add files via upload

--- C5-3/c5-3.js
+++ C5-3/c5-3.js
@@ -1,37 +1,33 @@
+
 function getImages(reqUrl){
     let imgList = [];
     xhr = new XMLHttpRequest();
     xhr.open("GET", reqUrl);
-    xhr.send();
+    
     xhr.onload = function(){
-        let i = 0;
         let jsonObj = JSON.parse(xhr.response);
         jsonObj.forEach(pic =>{
             imgList.push(pic.download_url);
         });
+        document.querySelector(".pictureContainer").innerHTML = drawImages(imgList);
+        
     }
+    xhr.send();
+    
+    return imgList;
 }
 function drawImages(imgList){
     let rez = '';
     let i = 0;
-    console.log(imgList);
-    for (i=0; i < imgList.length; i+=1){
-        console.log(imgList[i]);
-    }
-    // imgList.forEach(imgLink =>{
-        
-    //     // console.log('sdfdsf');
-    //     // rez += `<div><img src="${imgLink}" width = "${math.floor(100/length.imgList)-1}%"></img></div>`;
-    //     // console.log(rez);
-    // });
+    imgList.forEach(imgLink =>{
+        rez += `<div class="imgDiv"><img src="${imgLink}" class = "cover"></img></div>`;
+        // ${Math.floor(100/imgList.length)-1}
+    });
     return rez;
 
 }
 
 function init(){
-    let arr = new Array();
-    arr = ["1","2","3","4"];
-    console.log(arr, arr.length, arr[2]);
     document.querySelector(".sendDataButton").addEventListener("click", (e)=>{
         e.preventDefault();
         const value = Number(document.querySelector(".inputData").value);
@@ -39,9 +35,7 @@ function init(){
             if (value >=1 && value <= 10){
                 document.querySelector(".rezultMsg").innerHTML = "";
                 reqUrl = `https://picsum.photos/v2/list?limit=${value}`
-                let imgList = getImages(reqUrl);
-                // console.log(imgList.length);
-                document.querySelector(".pictureContainer").innerHTML = drawImages(imgList);
+                getImages(reqUrl);
             }
             else{
                 document.querySelector(".rezultMsg").innerHTML = "число вне диапазона от 1 до 10";
