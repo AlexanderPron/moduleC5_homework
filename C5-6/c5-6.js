e5c64bb65dd7cad22f1049505a5461cd80fbf72a
AlexanderPron <46918613+AlexanderPron@users.noreply.github.com>
2020-09-18 16:40:39 +0300

    Add files via upload

--- C5-6/c5-6.js
+++ C5-6/c5-6.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+
+async function getImage(reqUrl){
+    return const rez = await fetch(reqUrl)
+    .then((response) => {
+        console.log(response.json);
+        return response.json;
+    })
+    // .then())
+}
+function drawImage(imgList){
+    console.log('Отображаем картинку');
+}
+
+function init(){
+    document.querySelector(".sendDataButton").addEventListener("click", (e)=>{
+        e.preventDefault();
+        const value1 = Number(document.querySelector(".inputData1").value);
+        const value2 = Number(document.querySelector(".inputData2").value);
+        if (value1 && value2){
+            if ((value1 >=100 && value1 <= 300)&&(value2 >=100 && value2 <= 300)){
+                document.querySelector(".rezultMsg").innerHTML = "";
+                reqUrl = `https://picsum.photos/${value1}/${value2}`
+                getImage(reqUrl);
+            }
+            else{
+                document.querySelector(".rezultMsg").innerHTML = "одно из чисел вне диапазона от 100 до 300";
+            }
+        }
+        else{
+            document.querySelector(".rezultMsg").innerHTML = "Введено не число";
+        }
+    });
+}
+init();
